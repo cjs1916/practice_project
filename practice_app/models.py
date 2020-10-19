27fe595e1420ece586c2d31dd86197b6afe9bd99
Cameron Spano <spano.cameronwork@yahoo.com>
2020-10-19 15:35:50 -0500

    initial commit

---
+++ practice_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+class User(models.Model):
+    username = models.CharField(max_length=69)
+    email = models.CharField(max_length=45)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class GameInstance:
+    gold = models.IntegerField()
+    score = models.IntegerField()
+    round = 
